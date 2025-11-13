b7f3d5cad7229d2807dcfd66ae41a43d0bab9dda
Sharvini07 <SHARVINI.MOHAN@student.mmu.edu.my>
2025-11-13 03:24:50 +0000

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import clips
+import logging
 
+
+
+env = clips.Environment()
+logging.basicConfig(level=15,format='%(message)s')
+router = clips.LoggingRouter()
+env.add_router(router)
+
+#input
+name = st.text_input("Enter your name3")
+
+#knowledge base
+env.build('deftemplate result(slot name))')
+
+env.assert_string(f'(result(name "){name}"))')
+
+#interface
+env.run()
+
+#output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'results' :
+        results.append(fact['name']) #Why assert the fact?
+
+st.write(results[0],"outputs")
